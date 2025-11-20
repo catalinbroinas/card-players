11fe5720fcce6a0c48819f4a8d20eb3665dbd3ad
Cătălin Broinaș <43112698+catalinbroinas@users.noreply.github.com>
2025-11-20 12:00:31 +0200

    Initial commit

---
+++ src/react/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="container text-center">
+      <h1 className="mb-xxl-11 mb-xl-9 mb-lg-7 mb-md-5 mb-sm-2 mb-1">React work</h1>
+      <h2 className="mb-lg2">Test spacing</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fuga?</p>
+    </div>
+  )
+}
+
+export default App
