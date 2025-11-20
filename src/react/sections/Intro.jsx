d24738afcbc8586769ef3f62d51eb764eeba2048
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-20 12:54:01 +0200

    feat(Intro): add static UI

---
+++ src/react/sections/Intro.jsx
@@ -0,0 +1,13 @@
+
+function Intro() {
+  return(
+    <section className="intro">
+      <h1 className="intro__title">Card players</h1>
+      <p className="intro__text">
+        All card player added. All card player update.
+      </p>
+    </section>
+  );
+}
+
+export default Intro;
