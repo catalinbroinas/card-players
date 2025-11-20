a2139b1ff26af8e2223080ad14f745875613f6b2
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-20 12:38:07 +0200

    feat(App): clean up and add app structure

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,10 +1,10 @@
 
 function App() {
   return (
-    <div className="container text-center">
-      <h1 className="mb-xxl-11 mb-xl-9 mb-lg-7 mb-md-5 mb-sm-2 mb-1">React work</h1>
-      <h2 className="mb-lg2">Test spacing</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, fuga?</p>
+    <div className="page-layout">
+      <header></header>
+      <main className="container"></main>
+      <footer></footer>
     </div>
   )
 }
