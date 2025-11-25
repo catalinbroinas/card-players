ef1bd032916a160377af1c4a49bb5ad26c41ddea
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-25 09:52:13 +0200

    feat(PlayerCard): make component dynamic via props

--- src/react/components/features/players/PlayerCard.jsx
+++ src/react/components/features/players/PlayerCard.jsx
@@ -1,29 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFile, faCalendarDay } from "@fortawesome/free-solid-svg-icons";
-import declanRice from "../../../../assets/images/players/rice.webp";
 
-function PlayerCard() {
+function PlayerCard({ imageUrl, name, team, status, lastDate }) {
   return (
     <div className="card card-player">
       <div className="card-image">
-        <img src={declanRice} alt="" className="img-fluid" />
+        <img src={imageUrl} alt={`Photo of ${name}`} className="img-fluid" />
         <div className="mask mask-light"></div>
       </div>
 
       <div className="card-header text-center">
-        <h2 className="card-title">Declan Rice</h2>
-        <h3 className="card-subtitle">Arsenal</h3>
+        <h2 className="card-title">{name}</h2>
+        <h3 className="card-subtitle">{team}</h3>
       </div>
 
       <div className="card-body">
         <p className="card-text">
           <FontAwesomeIcon icon={faFile} className="me-2" />
-          Added
+          {status}
         </p>
 
         <p className="card-text">
           <FontAwesomeIcon icon={faCalendarDay} className="me-2" />
-          04 November 2025
+          {lastDate}
         </p>
       </div>
     </div>
