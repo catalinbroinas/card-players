b63c7f11625531bb18279f79ed0a72639f154e15
catalinbroinas <catalinbroinas@yahoo.com>
2025-11-27 10:47:39 +0200

    refactor(MainContent): derive default state from data
    - Initialize filterStatus and sortStatus from data source
    - Used first option from arrays

--- src/react/components/layout/MainContent.jsx
+++ src/react/components/layout/MainContent.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Toolbar from "../features/toolbar/Toolbar";
 import PlayerList from "../features/players/PlayerList";
 import players from "../../../js/data/players";
+import displayOptions from "../../data/displayOptions";
+import sortOptions from "../../data/sortOptions";
 import { filterPlayers, sortPlayers, searchPlayers } from "../../utils/playerUtils";
 
 function MainContent() {
   // States
-  const [filterStatus, setFilterStatus] = useState('all');
-  const [sortStatus, setSortStatus] = useState('default');
+  const [filterStatus, setFilterStatus] = useState(displayOptions[0].id);
+  const [sortStatus, setSortStatus] = useState(sortOptions[0].id);
   const [searchQuery, setSearchQuery] = useState('');
 
   // Processing
